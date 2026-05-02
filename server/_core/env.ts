4bef1492585f030e28d4920bd15c65aac6c1af94
zhenyakn <55668162+zhenyakn@users.noreply.github.com>
2026-05-02 10:10:05 +0300

    Update env.ts

--- server/_core/env.ts
+++ server/_core/env.ts
@@ -7,4 +7,5 @@ export const ENV = {
   isProduction: process.env.NODE_ENV === "production",
   forgeApiUrl: process.env.BUILT_IN_FORGE_API_URL ?? "",
   forgeApiKey: process.env.BUILT_IN_FORGE_API_KEY ?? "",
+  noAuth: process.env.NO_AUTH === "true",
 };
